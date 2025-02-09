52a7dba7c9147a929e3e6499c32b486ee8d67647
hikalium <hikalium@hikalium.com>
2025-02-09 23:16:55 +0900

    update

--- tools/voice_recorder/index.js
+++ tools/voice_recorder/index.js
@@ -14,39 +14,34 @@ class VoiceRecorder {
   }
   startStop() {
     if (this.isRecording) {
-      console.log("stop")
-      this.stopRecording()
+      console.log("stop") this.stopRecording()
     } else {
-      console.log("start")
-      this.startRecording()
+      console.log("start") this.startRecording()
     }
   }
   async selectTab() {
     let stream = await startCapture();
     console.log("tab stream:", stream);
-      window.VoiceVisualiser.connectMediaStream(stream);
+    window.VoiceVisualiser.connectMediaStream(stream);
     this.currentTabAudioTrack = stream.getAudioTracks()[0];
     console.log(this.currentTabAudioTrack);
   }
   async startRecording() {
-    if (this.isRecording) return;
+    if (this.isRecording)
+      return;
     this.isRecording = true;
     this.recordButtonRef.innerHTML = 'Stop';
-    this.stream.oninactive = () => {
-      console.log('Stream ended!')
-    };
+    this.stream.oninactive = () => { console.log('Stream ended!') };
     this.recorderRef.srcObject = this.stream;
     const options = {
-      audioBitsPerSecond: 1411 * 1000,
-      mimeType: 'audio/wav',
+      audioBitsPerSecond : 1411 * 1000,
+      mimeType : 'audio/wav',
     };
     this.mediaRecorder = new MediaRecorder(this.stream)
     console.log(this.mediaRecorder);
-    this.mediaRecorder.ondataavailable = (e) => {
-      this.chunks.push(e.data);
-    };
+    this.mediaRecorder.ondataavailable = (e) => { this.chunks.push(e.data); };
     this.mediaRecorder.onstop = (e) => {
-      const blob = new Blob(this.chunks, {'type': 'audio/wav'});
+      const blob = new Blob(this.chunks, {'type' : 'audio/wav'});
       console.log(blob);
       this.lastRecordBlob = blob;
     };
@@ -54,7 +49,8 @@ class VoiceRecorder {
     this.mediaRecorder.start();
   }
   stopRecording() {
-    if (!this.isRecording) return;
+    if (!this.isRecording)
+      return;
     this.isRecording = false;
     this.recordButtonRef.innerHTML = 'Start';
     this.recorderRef.pause();
@@ -72,31 +68,32 @@ class VoiceRecorder {
         const sourceAudio = audioCtx.createBufferSource();
         {
           // mono to stereo
-        sourceAudio.buffer = sourceAudioBuffer;
+          sourceAudio.buffer = sourceAudioBuffer;
           const splitter = audioCtx.createChannelSplitter(2);
           sourceAudio.connect(splitter);
 
-        splitter.connect(merger, 0, 0);
-        splitter.connect(merger, 0, 1);
+          splitter.connect(merger, 0, 0);
+          splitter.connect(merger, 0, 1);
         }
 
-        if(this.currentTabAudioTrack !== undefined) {
+        if (this.currentTabAudioTrack !== undefined) {
           const tabMediaStream = new MediaStream();
           tabMediaStream.addTrack(this.currentTabAudioTrack);
           const tabAudioElement = new Audio();
           tabAudioElement.srcObject = tabMediaStream;
           tabAudioElement.muted = true;
           tabAudioElement.play();
-          const tabAudioNode = audioCtx.createMediaStreamSource(tabAudioElement.captureStream());
+          const tabAudioNode =
+              audioCtx.createMediaStreamSource(tabAudioElement.captureStream());
           console.log(tabAudioNode);
           tabAudioNode.connect(merger);
         }
 
-        //const gainNodeL = audioCtx.createGain();
-        //const gainNodeR = audioCtx.createGain();
-        //gainNodeL.connect(merger, 0, 0);
-        //gainNodeR.connect(merger, 0, 1);
-        //merger.connect(audioCtx.destination);
+        // const gainNodeL = audioCtx.createGain();
+        // const gainNodeR = audioCtx.createGain();
+        // gainNodeL.connect(merger, 0, 0);
+        // gainNodeR.connect(merger, 0, 1);
+        // merger.connect(audioCtx.destination);
         merger.connect(audioCtx.destination);
         console.log(sourceAudio);
         sourceAudio.start();
@@ -135,7 +132,6 @@ class VoiceVisualiser {
       let sliceWidth = WIDTH * 1.0 / bufferLength;
       let x = 0;
 
-
       for (let i = 0; i < bufferLength; i++) {
         let v = dataArray[i] / 128.0;
         let y = v * HEIGHT / 2;
@@ -161,22 +157,19 @@ class VoiceVisualiser {
   }
 }
 
-
 const constraints = {
-  audio: true,
-  video: false
+  audio : true,
+  video : false
 };
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia(constraints)
-    .then((stream) => {
-      window.voiceRecorder = new VoiceRecorder(stream);
-      window.VoiceVisualiser = new VoiceVisualiser();
-      window.VoiceVisualiser.connectMediaStream(stream);
-      this.stream = stream;
-    })
-    .catch((e) => {
-      console.error('getUserMedia failed: ', e);
-    });
+      .then((stream) => {
+        window.voiceRecorder = new VoiceRecorder(stream);
+        window.VoiceVisualiser = new VoiceVisualiser();
+        window.VoiceVisualiser.connectMediaStream(stream);
+        this.stream = stream;
+      })
+      .catch((e) => { console.error('getUserMedia failed: ', e); });
 } else {
   console.error('getUserMedia is not supported on your browser!');
 }
@@ -184,12 +177,12 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 async function startCapture() {
   let captureStream = null;
   const displayMediaOptions = {
-    video: true,
-    audio: true,
+    video : true,
+    audio : true,
   };
   try {
     captureStream =
-      await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+        await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
   } catch (err) {
     console.error(`Error: ${err}`);
   }
